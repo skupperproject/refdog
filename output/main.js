e924a3d05641a8a495df94b029fa5eeb2998f628
Justin Ross <jross@apache.org>
2024-06-02 06:58:07 -0400

    WIP

--- output/main.js
+++ output/main.js
@@ -29,25 +29,104 @@ Element.prototype.$$ = function () {
 // window.$ = $;
 // window.$$ = $$;
 
-document.addEventListener("DOMContentLoaded", function() {
-    function showSectionFromFragment() {
-        const sections = $$("section");
-        const fragment = window.location.hash.substring(1);
+// document.addEventListener("DOMContentLoaded", function() {
+//     function showSectionFromFragment() {
+//         const sections = $$("section");
+//         const fragment = window.location.hash.substring(1);
 
-	for (const section of sections) {
-	    if (section.$("h3")) {
-                if (section.$("h3").id === fragment) {
-                    section.classList.remove("hidden");
-                } else {
-                    section.classList.add("hidden");
-                }
-	    }
+// 	for (const section of sections) {
+// 	    if (section.$("h3")) {
+//                 if (section.$("h3").id === fragment) {
+//                     section.classList.remove("hidden");
+//                 } else {
+//                     section.classList.add("hidden");
+//                 }
+// 	    }
+//         }
+//     }
+
+//     // Show the section on initial load
+//     showSectionFromFragment();
+
+//     // Show the section whenever the hash changes
+//     window.addEventListener("hashchange", showSectionFromFragment);
+// });
+
+window.addEventListener("load", () => {
+    const oldToc = $("#-toc");
+
+    if (!oldToc) {
+        return;
+    }
+
+    const headings = $$("h2");
+
+    if (headings.length == 0) {
+        return;
+    }
+
+    const newToc = document.createElement("section");
+    const newTocHeading = document.createElement("h4");
+    const newTocHeadingText = document.createTextNode("Contents");
+
+    newTocHeading.appendChild(newTocHeadingText);
+    newToc.appendChild(newTocHeading);
+    newToc.setAttribute("id", "-toc");
+
+    const newTocLinks = document.createElement("nav");
+
+    for (const heading of headings) {
+        const link = document.createElement("a");
+        const text = document.createTextNode(heading.textContent);
+
+        link.setAttribute("href", `#${heading.id}`);
+        link.appendChild(text);
+
+        newTocLinks.appendChild(link);
+    }
+
+    newToc.appendChild(newTocLinks);
+    oldToc.replaceWith(newToc);
+});
+
+window.addEventListener("load", () => {
+    const tocLinks = $("#-toc nav");
+
+    if (!tocLinks) {
+        return;
+    }
+
+    const updateHeadingSelection = () => {
+        const currHash = window.location.hash;
+
+        if (!currHash) {
+            return;
+        }
+
+        for (const link of tocLinks.$$("a")) {
+            const linkHash = new URL(link.href).hash;
+
+            if (linkHash === currHash) {
+                link.classList.add("selected");
+            } else {
+                link.classList.remove("selected");
+            }
         }
     }
 
-    // Show the section on initial load
-    showSectionFromFragment();
+    window.addEventListener("load", updateHeadingSelection);
+    window.addEventListener("hashchange", updateHeadingSelection);
+});
 
-    // Show the section whenever the hash changes
-    window.addEventListener("hashchange", showSectionFromFragment);
+window.addEventListener("load", () => {
+    const updateScrollState = () => {
+        if (window.scrollY > 20) {
+            $("body").classList.add("scrolled");
+        } else {
+            $("body").classList.remove("scrolled");
+        }
+    };
+
+    updateScrollState();
+    window.addEventListener("scroll", updateScrollState);
 });
