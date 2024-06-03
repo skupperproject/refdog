c7d0a8e6f6b4555366054a1d98ea47db45133fd9
Justin Ross <jross@apache.org>
2024-06-03 12:27:31 -0400

    WIP

--- config/config.py
+++ config/config.py
@@ -1,3 +1,9 @@
 import os
 
 site_prefix = os.environ.get("SITE_PREFIX", "")
+
+def path_nav(page):
+    files = reversed(list(page.ancestors))
+    links = [f"<a href=\"{site_prefix}{x.url}\">{x.title}</a>" for x in files]
+
+    return f"<nav class=\"path-nav\">{''.join(links)}</nav>"
